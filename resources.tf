e5111aae50dfa250a4cca659a45956a1e9e1ab8b
yoni101087 <yoni101087@gmail.com>
2025-01-05 17:31:45 +0200

    clear names

--- resources.tf
+++ resources.tf
@@ -1,28 +1,28 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "storageaccount" {
   name                     = var.storage_account_name
-  resource_group_name     = azurerm_resource_group.example.name
-  location                = azurerm_resource_group.example.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  location                = azurerm_resource_group.rg.location
   account_tier            = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_service_plan" "serviceplan" {
+  name                = "serviceplan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_application_insights" "example" {
+resource "azurerm_application_insights" "appinsight" {
   name                = var.app_insights_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   application_type    = "web"
 }
 
@@ -37,22 +37,14 @@ resource "azurerm_application_insights" "example" {
 #  }
 #}
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-}
 
 resource "azurerm_linux_function_app" "example" {
   name                = "example-linux-function-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  service_plan_id            = azurerm_app_service_plan.example.id
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  storage_account_name       = azurerm_storage_account.storageaccount.name
+  storage_account_access_key = azurerm_storage_account.storageaccount.primary_access_key
+  service_plan_id            = azurerm_service_plan.serviceplan.id
   #app_settings = {
   #  "FUNCTIONS_WORKER_RUNTIME" = "python" # Change according to your function's runtime
   #  "APPLICATIONINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.example.instrumentation_key
