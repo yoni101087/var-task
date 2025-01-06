7c4e06c98586a94ad10f1a8995f26c48fe33bbc9
yoni101087 <yoni101087@gmail.com>
2025-01-06 10:50:19 +0200

    update provider

--- provider.tf
+++ provider.tf
@@ -1,7 +1,11 @@
 provider "azurerm" {
-    features {}
-    subscription_id = "4107cdbb-533c-44bb-abd4-780bed5efb7c"
-    tenant_id = "6085d3ae-2d96-4fd9-98f0-0751a37c4a90"
- }
+  subscription_id = "4107cdbb-533c-44bb-abd4-780bed5efb7c"
+  tenant_id = "6085d3ae-2d96-4fd9-98f0-0751a37c4a90"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+     }
 
+ }
+}
 
